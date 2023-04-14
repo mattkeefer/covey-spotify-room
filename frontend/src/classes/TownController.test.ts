ab67e7bc1cbb433066f12af5259c08d3907c7848
Matt Keefer <mrkeefer8@gmail.com>
2023-04-13 21:46:36 -0400

    Revert "Merge branch 'main' into songarea-basement"
    This reverts commit ccd00c64a193fe664c1482eb62022a1474ac7f75, reversing
    changes made to 42ebd6fff788c5af2f4db5d8829d4d7d5522bb58

--- frontend/src/classes/TownController.test.ts
+++ frontend/src/classes/TownController.test.ts
@@ -585,13 +585,17 @@ describe('TownController', () => {
       await expect(testController.getSpotifyTopSongs()).resolves.toEqual(mockTracks);
       expect(axiosSpy).toBeCalledTimes(1);
     });
-    it('Creates a new playlist successfully', async () => {
-      jest.resetAllMocks();
-      const spotifySpy = jest.spyOn(spotifyApi.playlists, 'createPlaylist');
-      spotifySpy.mockResolvedValue(mockSpotifyPlaylist);
-      await expect(testController.createSpotifyPlaylist()).resolves.toEqual(mockPlaylist);
-      expect(spotifySpy).toBeCalledTimes(1);
-    });
+    // it('Creates a new playlist successfully', async () => {
+    //   jest.resetAllMocks();
+    //   const spotifySpy = jest.spyOn(spotifyApi.playlists, 'createPlaylist');
+    //   spotifySpy.mockResolvedValue(mockSpotifyPlaylist);
+    //   await expect(testController.createSpotifyPlaylist()).resolves.toEqual(mockPlaylist);
+    //   expect(spotifySpy).toBeCalledTimes(1);
+    //   expect(spotifySpy).toBeCalledWith('mknexus8', 'Covey Town', {
+    //     collaborative: true,
+    //     public: true,
+    //   });
+    // });
     it('Can add tracks to a playlist', async () => {
       jest.resetAllMocks();
       const spotifySpy = jest.spyOn(spotifyApi.playlists, 'addItemsToPlaylist');
